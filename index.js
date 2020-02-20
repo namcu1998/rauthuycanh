c345f704b5476089366d6b0e91d3b38963a38b92
Namcu1998 <namcu1998@gmail.com>
2020-02-19 23:13:51 -0800

    make it better

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.set("views","./views");
 var server = require("http").Server(app);
 var io = require("socket.io")(server)
 server.listen(process.env.PORT || 3000); //process.env.PORT ||
+var webapp_nsp = io.of('/webapp')				//namespace của webapp
+var esp8266_nsp = io.of('/esp8266')
+var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn bộ lệnh!
+esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
+webapp_nsp.use(middleware);
 io.on("connection", function(socket){
   socket.on("name", function(data){
     if(data = '1'){
