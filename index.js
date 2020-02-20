f2cc29e29132320df334f58dfbc1acb815eee270
Namcu1998 <namcu1998@gmail.com>
2020-02-19 23:03:24 -0800

    make it better

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ app.use(express.static("./public"));
 app.set("view engine","ejs");
 app.set("views","./views");
 var server = require("http").Server(app);
-var io = require("socket.io")
-io.listen(process.env.PORT || 3000);
+var io = require("socket.io")(server)
 server.listen(process.env.PORT || 3000); //process.env.PORT ||
 io.on("connection", function(socket){
   socket.on("name", function(data){
