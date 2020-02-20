c23dc2394595b8fb8ab5c27924d18bc3c4e6defc
Namcu1998 <namcu1998@gmail.com>
2020-02-20 00:53:18 -0800

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const PORT = 3000;									//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
-
 var http = require('http');
 var express = require('express');							//#include thư viện express - dùng để tạo server http nhanh hơn thư viện http cũ
 var socketio = require('socket.io')				//#include thư viện socketio
@@ -8,18 +6,17 @@ var ip = require('ip');
 var app = express();									//#Khởi tạo một chương trình mạng (app)
 var server = http.Server(app)
 
-var io = socketio(server);								//#Phải khởi tạo io sau khi tạo app
+var io = socketio(server);
+app.use(express.static("./public"));
+app.set("view engine","ejs");
+app.set("views","./views");
 
+server.listen(process.env.PORT || 3000); //process.env.PORT ||
 var webapp_nsp = io.of('/webapp')				//namespace của webapp
-
-var esp8266_nsp = io.of('/esp8266')				//namespace của esp8266
-
+var esp8266_nsp = io.of('/esp8266')
 var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn bộ lệnh!
 esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
-webapp_nsp.use(middleware);									//Khi webapp emit bất kỳ lệnh gì lên thì sẽ bị bắt
-
-server.listen(process.env.PORT || PORT);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
-console.log("Server nodejs chay tai dia chi: " + ip.address() + ":" + PORT)
+webapp_nsp.use(middleware);
 io.on("connection", function(socket){
 
   socket.on("nam", function(data){
