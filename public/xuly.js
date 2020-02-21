d8d8c2b9735ed24bbe5b2e5413d77dc524ad06ca
Namcu1998 <namcu1998@gmail.com>
2020-02-21 01:35:52 -0800

    make it better

--- public/xuly.js
+++ public/xuly.js
@@ -1,6 +1,11 @@
 var socket = io("https://namcu1998.herokuapp.com")   //("https://namcu1998.herokuapp.com");
 $(document).ready(function(){
-
+  $("#onden").click(function(){
+    socket.emit("mess", "den1on");
+  })
+  $("#offden").click(function(){
+    socket.emit("mess", "den1off");
+  })
    socket.emit("name","1");
   $("#button").click(function(){
     socket.emit("mess", $("#nhap").val());
