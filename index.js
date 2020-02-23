233660668bb2e3cf82a5a33e9c2454880de675fa
Namcu1998 <namcu1998@gmail.com>
2020-02-22 22:20:00 -0800

    make it better

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ function ParseJson(jsondata) {
     io.on('connection', function(socket) {
   console.log("Connected");
   /////////////////////////////////////////////////////////
-  socket.on("onden", function() {
+  socket.on("onden", function(data) {
     mang.splice(0 , 1 , '0');
-    io.sockets.emit("LED",led);
+    io.sockets.emit("LED","nam");
   });//onLED
 ///////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////
