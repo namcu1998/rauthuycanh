dd9d3eeea5d09e7cc224e9f9f70c7c2c837c79e6
Namcu1998 <namcu1998@gmail.com>
2020-02-23 22:32:51 -0800

    make it better

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn 
 esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
 webapp_nsp.use(middleware);
 io.on('connection', function(socket) {
-    socket.on("JSON", function(data){
-     sockets.broadcast.emit("user",data["time"]);
-   })
+
 })
 
 app.get("/", function(req , res){
