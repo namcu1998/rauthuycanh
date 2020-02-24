12a71dc9b8998cb669c10d0054464c774d44a88d
Namcu1998 <namcu1998@gmail.com>
2020-02-23 22:18:23 -0800

    make it better

--- index.js
+++ index.js
@@ -24,11 +24,10 @@ var mang = {
     io.on('connection', function(socket) {
     console.log("Connected");
     socket.on("den1on", function(data){
-       mang.splice(0,1,'0');
        socket.broadcast.emit("LED",data);
     })
     socket.on("den1off", function(data){
-       mang.splice(0,1,'1');
+
        socket.broadcast.emit("LED",mang);
     })
     socket.on("JSON", function(data){
