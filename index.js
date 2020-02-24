4dc20e709cf2825a5d4f3e541c092ed537ef2d13
Namcu1998 <namcu1998@gmail.com>
2020-02-23 22:23:54 -0800

    make it better

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ var esp8266_nsp = io.of('/esp8266')				//namespace của esp8266
 var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn bộ lệnh!
 esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
 webapp_nsp.use(middleware);
-var led = [1]
-var mang = {
-   "led":led,
-}
+
     io.on('connection', function(socket) {
     console.log("Connected");
+    var led = [1]
+    var mang = {
+       "led":led,
+    }
     socket.on("den1on", function(data){
        socket.broadcast.emit("LED",data);
     });
