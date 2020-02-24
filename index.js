9525adea9d5b3de724267abf7678d39c58d6e02d
Namcu1998 <namcu1998@gmail.com>
2020-02-23 20:35:43 -0800

    make it better

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì l�
 webapp_nsp.use(middleware);
     io.on('connection', function(socket) {
     console.log("Connected");
-    socket.on("atime", function(){
+    socket.on("JSON", function(){
      io.sockets.emit("user","nam");
     })
 	socket.on('disconnect', function() {
