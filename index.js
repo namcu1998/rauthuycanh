bc0a668989370e1758f08bc7980c09c45fa1bf09
Namcu1998 <namcu1998@gmail.com>
2020-02-24 01:23:49 -0800

    make it better

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ webapp_nsp.use(middleware);
     }
     socket.on("JSON", function(data){
      socket.broadcast.emit("user",data);
-     led.splice(0 , 2);
-     led.splice(0 , 1, data["den1"]);
-     led.splice(1 , 1, data["den2"]);
+     led.splice(0 , 2 , "data["den1"]" , "data["den2"]"");
    });
     socket.on("offden", function(){
       led.splice(0,1,'1');
