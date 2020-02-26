aea0498d5c9ff2496d23914f23779382930d0eee
Namcu1998 <namcu1998@gmail.com>
2020-02-26 01:18:50 -0800

    make it better

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ webapp_nsp.use(middleware);
     /////////////////////////////////////////////////////////
     socket.on("onden", function(data) {
       led.splice(0,1,0);
-      socket.broadcast.emit("LED",mang);
+      socket.broadcast.emit("LED",mang1);
     });
     socket.on("offden", function(){
       led.splice(0,1,1);
