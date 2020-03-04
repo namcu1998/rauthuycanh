00b8f846f6d4ce69da809399e171e60100f47550
Namcu1998 <namcu1998@gmail.com>
2020-03-04 00:43:31 -0800

    make it better

--- public/xuly.js
+++ public/xuly.js
@@ -18,8 +18,8 @@ $(document).ready(function(){
   socket.on("dulieu1", function(data){
     document.getElementById("temp1").innerHTML = data["time"];
     document.getElementById("humi1").innerHTML = data["time1"];
-    document.getElementById("mess3").innerHTML = data["den1"];
-    document.getElementById("mess4").innerHTML = data["den2"];
+    //document.getElementById("mess3").innerHTML = data["den1"];
+    //document.getElementById("mess4").innerHTML = data["den2"];
     led.splice(2,1,data["den1"]);
     led.splice(3,1,data["den2"]);
   });
