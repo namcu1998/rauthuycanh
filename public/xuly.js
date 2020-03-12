ae25309a2a78ef6bbe01e564262dba3cbe2ab8dc
Namcu1998 <namcu1998@gmail.com>
2020-03-12 08:01:21 -0700

    make it better

--- public/xuly.js
+++ public/xuly.js
@@ -8,18 +8,7 @@ $(document).ready(function(){
   $("#login").show();
   $("#Sensor1").hide();
   $("#button1").hide();
-  $("#btnbutton2").click(function(){
-    $("#Sensor").hide();
-    $("#button").hide();
-    $("#Sensor1").show(100);
-    $("#button1").show(100);
-  });
-  $("#btnbutton1").click(function(){
-    $("#Sensor").show(100);
-    $("#button").show(100);
-    $("#Sensor1").hide();
-    $("#button1").hide();
-  });
+
   /////////////////////////////////////////////
   var led = [1,1,1,1];
   var mang = {
@@ -34,6 +23,19 @@ $(document).ready(function(){
   socket.on("loginfail",function(){
     alert("tài khoản hoặc mật khẩu không đúng");
     datauser.splice(0,10);
+  });
+  /////////////////////////////////////////////
+  $("#btnbutton2").click(function(){
+    $("#Sensor").hide();
+    $("#button").hide();
+    $("#Sensor1").show(100);
+    $("#button1").show(100);
+  });
+  $("#btnbutton1").click(function(){
+    $("#Sensor").show(100);
+    $("#button").show(100);
+    $("#Sensor1").hide();
+    $("#button1").hide();
   });
   /////////////////////////////////////////////
   $("#btnlogin").click(function(){
