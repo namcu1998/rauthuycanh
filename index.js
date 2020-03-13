4061f10b78d71919df02250451b4599545d05d1d
Namcu1998 <namcu1998@gmail.com>
2020-03-13 02:00:19 -0700

    make it better

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ console.log('Connection has been established successfully.');
 .catch(err => {
 console.error('Unable to connect to the database:', err);
 });
+User.sync({ force: true });
     io.on('connection', function(socket) {
     console.log("Connected");
 
