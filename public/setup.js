b62c5a9925f32077c163c6ab331910e98c9fa217
Namcu1998 <namcu1998@gmail.com>
2020-03-17 20:51:58 -0700

    make it better

--- public/setup.js
+++ public/setup.js
@@ -1,8 +1,8 @@
+var socket = io("https://namcu.herokuapp.com");
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
-
     // The data for our dataset
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
