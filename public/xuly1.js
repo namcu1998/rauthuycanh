26ec57ac3101415b0b61cbfead044fac995311fe
Namcu1998 <namcu1998@gmail.com>
2020-03-17 22:55:21 -0700

    make it better

--- public/xuly1.js
+++ public/xuly1.js
@@ -2,3 +2,22 @@ var socket = io("https://namcu.herokuapp.com");
 socket.on("dulieu", function(data){
   document.getElementById("temp").innerHTML = data["time"];
 });
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45]
+      }]
+  },
+
+  // Configuration options go here
+  options: {}
+});
