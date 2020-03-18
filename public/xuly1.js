c2fa0ea599cb3307a4cb98df3863a52189f493c2
Namcu1998 <namcu1998@gmail.com>
2020-03-18 07:51:19 -0700

    make it better

--- public/xuly1.js
+++ public/xuly1.js
@@ -1,30 +1,4 @@
 var socket = io("https://namcu.herokuapp.com");
 socket.on("dulieu", function(data){
-  var newdata = [{time: time(), y: random()}];
-   myChart.push(newdata);
   document.getElementById("temp").innerHTML = data["time"];
 });
-function time(){
-  var dt = new Date();
-  var miliSec = dt.getTime();
-  document.getElementById("demo1").innerHTML = dt.getTime();
-  return miliSec;
-};
-
-function random(){
-  var dem = Math.random() * 100;
-  return dem;
-};
-
-
-var areaChartData = [
-  {
-  label: "Layer 1",
-   values: [{time: time(), y: random()}],
-},
-];
-  var myChart = $('#myChart').epoch({
-   type: 'time.bar',
-   data: areaChartData,
-   axes: ['right', 'bottom', 'left']
-  });
