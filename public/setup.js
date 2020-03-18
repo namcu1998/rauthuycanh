503e3ae8e805306af2072cf66266698ee483b62e
Namcu1998 <namcu1998@gmail.com>
2020-03-17 20:56:53 -0700

    make it better

--- public/setup.js
+++ public/setup.js
@@ -1,4 +1,5 @@
 var socket = io("https://namcu.herokuapp.com");
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
