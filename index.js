e893acbf6f9ebfa40b8bd98352a621c9a4ad4232
Namcu1998 <namcu1998@gmail.com>
2020-03-18 07:16:16 -0700

    make it better

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ app.set("view engine","ejs");
 app.set("views","./views");
 app.use(express.static("webapp"))
 server.listen(process.env.PORT || 3000);
-var webapp_nsp = io.of('/webapp')
-var esp8266_nsp = io.of('/esp8266')
-var middleware = require('socketio-wildcard')();
-esp8266_nsp.use(middleware);
-webapp_nsp.use(middleware);
 var admin = require("firebase-admin");
 
 // Fetch the service account key JSON file contents
