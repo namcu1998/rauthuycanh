229f9250f3ee1a41d1d01ebf534a8cb12ca6f9d4
Namcu1998 <namcu1998@gmail.com>
2020-03-17 21:29:25 -0700

    make it better

--- public/setup.js
+++ public/setup.js
@@ -1,20 +1,4 @@
 var socket = io("https://namcu.herokuapp.com");
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-var ctx = document.getElementById('myChart').getContext('2d');
-$("#testbtn").click(function(){
-  $("#mychart1").append("nam");
+socket.on("dulieu", function(data){
+  document.getElementById("#mychart1").innerHTML = data["time"];
 });
-
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
