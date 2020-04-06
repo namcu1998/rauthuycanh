ce210b98a51676659dc5ae575b7c4ae0057b7a23
Namcu1998 <namcu1998@gmail.com>
2020-04-06 22:50:11 +0700

    make it better

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ ref.on('child_added', function(snapshot) {
      socket.on("JSON1",function(data){
        socket.broadcast.emit("dulieu1",data);
        fileSave(data.temp,data.humi,Date());
-       socket.emit("hmm",readFile());      
+       socket.emit("hmm","readFile()");      
      });//onJSON
      //nhận dữ liệu từ esp
     //////////////////////////////////////////////////////////
