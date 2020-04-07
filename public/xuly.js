dd76f5112b46e472335f8949a4596a63eccce692
Namcu1998 <namcu1998@gmail.com>
2020-04-07 22:05:05 +0700

    make it better

--- public/xuly.js
+++ public/xuly.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
   };
   function xulyData(array){
   for (var x of array){
-    $("#lichsu").append('<td>' + x.nhietdo + '</td>' + +'<td>' + x.doam + '</td>' + '<td>' + x.thoigian + '</td>');
+    $("#lichsu").append('<td>' + x.nhietdo + '</td>' +'<td>' + x.doam + '</td>' + '<td>' + x.thoigian + '</td>');
   };
   }
   //tạo một mảng led
