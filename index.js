2df8c252e1c159e8005b5e121ebe217a54ee0b45
Namcu1998 <namcu1998@gmail.com>
2020-04-08 21:42:09 +0700

    make it better

--- index.js
+++ index.js
@@ -28,6 +28,39 @@ ref.on('child_added', function(snapshot) {
   var message=snapshot.val();
      console.log(message);
 });
+var ngay = ngay();
+function ngay(){
+  var date = new Date();
+  var day1 = date.getDay()
+  switch (day1) {
+    case 0:
+      return 'thứ hai';
+    case 1:
+      return 'thứ ba';
+    case 2:
+      return 'thứ tư';
+    case 3:
+      return 'thứ năm';
+    case 4:
+      return 'thứ sáu';
+    case 5:
+      return 'thứ bảy';
+    case 6:
+      return 'chủ nhật';
+  }
+}
+function time(){
+  var date = new Date();
+  var array
+  var day = date.getDate()
+  var month = date.getMonth() + 1
+  var year = date.getFullYear()
+  var hours = date.getHours() + 7
+  var minutes = date.getMinutes()
+  var seconds = date.getSeconds()
+  array = (ngay + ' ' +  day + '-' + month + '-' + year + ' ' + hours + ':' + minutes + ':' + seconds)
+  return array;
+}
     function fileSave(nhietdo, doam, thoigian){
     var data = JSON.parse(fs.readFileSync('data.json','utf8'))
     function Object(nhietdo, doam, thoigian){
@@ -72,7 +105,7 @@ ref.on('child_added', function(snapshot) {
      });//onJSON
      socket.on("JSON1",function(data){
        socket.broadcast.emit("dulieu1",data);
-       fileSave(data.temp, data.humi, time);
+       fileSave(data.temp, data.humi, time());
        socket.broadcast.emit("hmm", readFile());
      });//onJSON
      //nhận dữ liệu từ esp
