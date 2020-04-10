9d16ce3721f9e3315e3720facc2c37331c982b39
Namcu1998 <namcu1998@gmail.com>
2020-04-10 22:42:48 +0700

    make it better

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ ref.on('child_added', function(snapshot) {
     this.doam = doam;
     this.thoigian = thoigian;
     }
-    if (data.length > 10){
-    data.splice(10,1)
+    if (data.length > 20){
+    data.splice(20,10)
     }
     data.unshift(new Object(nhietdo, doam, thoigian))
     var data1 = JSON.stringify(data);
