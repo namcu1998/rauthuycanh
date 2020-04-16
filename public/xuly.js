a09311bf8126f2e813d5c35be55645d3ebad32ba
Namcu1998 <namcu1998@gmail.com>
2020-04-16 21:41:10 +0700

    make it better

--- public/xuly.js
+++ public/xuly.js
@@ -94,16 +94,7 @@ $(document).ready(function(){
   //nhận dữ liệu từ server
 
 /////////////////////////////////////////////
-$('#toggle-event').change(function() {
-  if($(this).prop('checked') == true){
-    led.splice(0,1,0);
-    socket.emit("onden",mang);
-  }
-  else {
-    led.splice(0,1,1);
-    socket.emit("onden",mang);
-  }
-})
+
   /////////////////////////////////////////////
   $("#onden1").click(function(){
     led.splice(1,1,0);
