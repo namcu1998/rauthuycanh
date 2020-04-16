8bdb82f2d0f77ad22341680bb96488d243093437
Namcu1998 <namcu1998@gmail.com>
2020-04-16 22:07:58 +0700

    make it better

--- public/xuly.js
+++ public/xuly.js
@@ -77,7 +77,10 @@ $(document).ready(function(){
     led.splice(1,1,data["den2"]);
     led.splice(2,1,data["den3"]);
     led.splice(3,1,data["den4"]);
-      xulyden(led[0],$('#toggle-event'))
+    xulyden(led[0],$('#toggle-event'))
+    xulyden(led[1],$('#toggle-event1'))
+    xulyden(led[2],$('#toggle-event2'))
+    xulyden(led[3],$('#toggle-event3'))
   })
 
   /////////////////////////////////////////////
@@ -104,6 +107,34 @@ $('#toggle-event').change(function() {
     socket.emit("onden",mang);
   }
 })
-
-
+$('#toggle-event1').change(function() {
+  if($(this).prop('checked') == true){
+    led.splice(1,1,0);
+    socket.emit("onden",mang);
+  }
+  else {
+    led.splice(1,1,1);
+    socket.emit("onden",mang);
+  }
+})
+$('#toggle-event2').change(function() {
+  if($(this).prop('checked') == true){
+    led.splice(2,1,0);
+    socket.emit("onden",mang);
+  }
+  else {
+    led.splice(2,1,1);
+    socket.emit("onden",mang);
+  }
+})
+$('#toggle-event3').change(function() {
+  if($(this).prop('checked') == true){
+    led.splice(3,1,0);
+    socket.emit("onden",mang);
+  }
+  else {
+    led.splice(3,1,1);
+    socket.emit("onden",mang);
+  }
+})
 }); //document
