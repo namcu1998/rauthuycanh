7683a67c901d16b9caa5ef74aa13ed56d9907b24
Namcu1998 <namcu1998@gmail.com>
2020-05-06 20:13:01 +0700

    make it better

--- public/xuly.js
+++ public/xuly.js
@@ -9,11 +9,10 @@ function neu(data){
   else a ="tắt";
   return  a;
 }
+
 $(document).ready(function(){
   $("#home").show();
   $("#login").hide();
-  $("#Sensor1").hide();
-  $("#button1").hide();
   socket.emit("data");
   socket.emit("getled")
   /////////////////////////////////////////////
@@ -23,6 +22,7 @@ $(document).ready(function(){
   };
 
   var table = $("#lichsu");
+
   function xulyData(getid, array){
    var html = array.map(function(x){
      return '<tr>' + '<td>' + x.id + '</td>' + '<td>'+ 'nhiệt đô: ' + x.nhietdo + '*C' + '</td>' +'<td>' + 'độ ẩm: ' + x.doam + '%' +  '</td>' + '<td>' + x.thoigian + '</td>' + '</tr>';
@@ -30,48 +30,35 @@ $(document).ready(function(){
   var htmljoin = html.join('');
   getid.html(html);
   }
+
   function xulyden(item1, item2){
     if(item1 == 0){
       item2.bootstrapToggle('on')
     }
     else item2.bootstrapToggle('off')
   }
-  //tạo một mảng led
 
-
-  /////////////////////////////////////////////
   socket.on("logintrue",function(){
     $("#home").show(2000);
     $("#login").hide(1000);
   });
+
   socket.on("loginfail",function(){
     alert("tài khoản hoặc mật khẩu chưa đúng");
     datauser.splice(0,10);
   });
-  /////////////////////////////////////////////
-  $("#btnbutton2").click(function(){
-    $("#Sensor").hide();
-    $("#button").hide();
-    $("#Sensor1").show(100);
-    $("#button1").show(100);
-  });
-  $("#btnbutton1").click(function(){
-    $("#Sensor").show(100);
-    $("#button").show(100);
-    $("#Sensor1").hide();
-    $("#button1").hide();
-  });
-  /////////////////////////////////////////////
+
   $("#btnlogin").click(function(){
     datauser.push($("#username").val());
     datauser.push($("#password").val());
     socket.emit("login",login);
   });
-  /////////////////////////////////////////////
+
   socket.on("hmm",function(data){
     xulyData(table,data);
     document.getElementById("hitory").innerHTML = data.length;
-  })
+  });
+
   socket.on("led", function(data){
     led.splice(0,1,data["den1"]);
     led.splice(1,1,data["den2"]);
@@ -81,20 +68,18 @@ $(document).ready(function(){
     xulyden(led[1],$('#toggle-event1'))
     xulyden(led[2],$('#toggle-event2'))
     xulyden(led[3],$('#toggle-event3'))
-  })
+  });
 
-  /////////////////////////////////////////////
   socket.on("dulieu", function(data){
     document.getElementById("temp").innerHTML = data["temp"];
     document.getElementById("humi").innerHTML = data["humi"];
   });
+
   socket.on("dulieu1", function(data){
     document.getElementById("temp1").innerHTML = data["temp"];
     document.getElementById("humi1").innerHTML = data["humi"];
     });
-  //nhận dữ liệu từ server
 
-/////////////////////////////////////////////
 $('#toggle-event').change(function() {
   if($(this).prop('checked') == true){
     led.splice(0,1,0);
@@ -104,7 +89,8 @@ $('#toggle-event').change(function() {
     led.splice(0,1,1);
     socket.emit("onden",mang);
   }
-})
+});
+
 $('#toggle-event1').change(function() {
   if($(this).prop('checked') == true){
     led.splice(1,1,0);
@@ -114,7 +100,8 @@ $('#toggle-event1').change(function() {
     led.splice(1,1,1);
     socket.emit("onden",mang);
   }
-})
+});
+
 $('#toggle-event2').change(function() {
   if($(this).prop('checked') == true){
     led.splice(2,1,0);
@@ -124,7 +111,8 @@ $('#toggle-event2').change(function() {
     led.splice(2,1,1);
     socket.emit("onden",mang);
   }
-})
+});
+
 $('#toggle-event3').change(function() {
   if($(this).prop('checked') == true){
     led.splice(3,1,0);
@@ -134,5 +122,6 @@ $('#toggle-event3').change(function() {
     led.splice(3,1,1);
     socket.emit("onden",mang);
   }
-})
+});
+
 }); //document
