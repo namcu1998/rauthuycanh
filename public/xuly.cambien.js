3fbb9e75dd5e8ac034021fc1ab36a4e8df6f8518
Namcu1998 <namcu1998@gmail.com>
2020-06-13 10:17:05 +0700

    make it better

--- public/xuly.cambien.js
+++ public/xuly.cambien.js
@@ -1,5 +1,6 @@
 const socket = io("http://nam2351998.herokuapp.com/");
 $(document).ready(function(){
+    socket.emit("getData");
     socket.on("hmm",function(data){
         document.getElementById("temp1").innerHTML = data[0].nhietdo;
         document.getElementById("humi1").innerHTML = data[0].doam;
--- read.database/write.database.js
+++ read.database/write.database.js
@@ -1,18 +1,20 @@
 const fs  = require('fs')
+let newId;
 function fileSave(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year){
-    var data = JSON.parse(fs.readFileSync('data.json','utf8'))
-    function Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year){
+  var data = JSON.parse(fs.readFileSync('data.json','utf8'))
+  function Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, id){
+    this.id = id;
     this.nhietdo = nhietdo;
     this.doam = doam;
     this.light = light;
     this.thoigian = thing + " " + day + "/" + mouth + "/" + year + " " + hour + ":" + minute + ":" + second;
-    }
-    if(data.length > 49){
-      data.splice(49,1);
-    }
-    console.log(data.length)
-    data.unshift(new Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year))
-    var data1 = JSON.stringify(data);
-    fs.writeFileSync('data.json',data1);
-    }
-    module.exports = fileSave
+  }
+  if(data.length > 49){
+    data.splice(49,1);
+  }
+  console.log(data.length)
+  data.unshift(new Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, newId = data[0].id + 1))
+  var data1 = JSON.stringify(data);
+  fs.writeFileSync('data.json',data1);
+  }
+ module.exports = fileSave
