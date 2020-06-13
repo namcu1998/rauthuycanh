208c2ed76555dc3b89e091bede9770bbf7c8af2f
Namcu1998 <namcu1998@gmail.com>
2020-06-13 10:19:43 +0700

    make it better

--- public/xuly.lichsu.js
+++ public/xuly.lichsu.js
@@ -2,7 +2,7 @@ const socket = io("http://nam2351998.herokuapp.com/");
 const table = $("#lich");
 function xulyData(getid, array){
 	var html = array.map(function(x){
-		return '<tr>' + '<td>' + x.id + '</td>' + '<td>' + x.light+ '</td>' + '<td>'+ 'nhiệt đô: ' + x.nhietdo + '*C' + '</td>' +'<td>' + 'độ ẩm: ' + x.doam + '%' +  '</td>' + '<td>' + x.thoigian + '</td>' + '</tr>';
+		return '<tr>' + '<td>' + x.id + '</td>' + '<td>' + x.light + "lx" + '</td>' + '<td>'+ 'nhiệt đô: ' + x.nhietdo + '*C' + '</td>' +'<td>' + 'độ ẩm: ' + x.doam + '%' +  '</td>' + '<td>' + x.thoigian + '</td>' + '</tr>';
 	});
 	var htmljoin = html.join('');
 	$("#lich").html(htmljoin);
