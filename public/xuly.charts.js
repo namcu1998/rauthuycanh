67499ed1de7cc446c40f3b172e762b51e964cf79
Namcu1998 <namcu1998@gmail.com>
2020-06-14 13:04:49 +0700

    make it better

--- public/xuly.charts.js
+++ public/xuly.charts.js
@@ -27,25 +27,14 @@ $(document).ready(function(){
     });
 }); //document
 var myChart = new Chart(ctx, {
-    type: 'line',
+    type: 'bar',
     data: {
         labels: [],
         datasets: [
             {
                 label: 'light',
                 data: [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(141, 78, 78, 0.2)',
-                    'rgba(105, 58, 58, 0.2)',
-                    'rgba(71, 26, 26, 0.2)',
-                    'rgba(26, 38, 71, 0.2)',
-                ],
+                backgroundColor: 'rgba(196, 245, 0, 1)',
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
@@ -63,18 +52,7 @@ var myChart = new Chart(ctx, {
             {
                 label: 'Nhiệt độ',
                 data: [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(141, 78, 78, 0.2)',
-                    'rgba(105, 58, 58, 0.2)',
-                    'rgba(71, 26, 26, 0.2)',
-                    'rgba(26, 38, 71, 0.2)',
-                ],
+                backgroundColor: 'rgba(245, 0, 0, 1)',
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
@@ -92,18 +70,7 @@ var myChart = new Chart(ctx, {
             {
                 label: 'độ ẩm',
                 data: [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(141, 78, 78, 0.2)',
-                    'rgba(105, 58, 58, 0.2)',
-                    'rgba(71, 26, 26, 0.2)',
-                    'rgba(26, 38, 71, 0.2)',
-                ],
+                backgroundColor: 'rgba(139, 97, 255, 1)',
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
