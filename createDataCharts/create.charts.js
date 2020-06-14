146b6d1f33d0ee0c9d4584a7c44f403517f96c8f
Namcu1998 <namcu1998@gmail.com>
2020-06-14 09:33:31 +0700

    make it better

--- createDataCharts/create.charts.js
+++ createDataCharts/create.charts.js
@@ -1,6 +1,7 @@
 const fs  = require('fs')
-module.exports =  function(){
+module.exports =  function() {
 	let dataLight = [];
+	let dataTemp = [];
 	let dataTime = [];
 	var dataJson = [
 		{
@@ -8,6 +9,9 @@ module.exports =  function(){
 		},
 		{
 			dataTime: dataTime,
+		},
+		{
+			dataTemp: dataTemp,
 		}
 	];
 
@@ -15,12 +19,15 @@ module.exports =  function(){
 	data.map(function(item){
 		dataLight.push(item.light);
 		dataTime.push(item.thoigian);
+		dataTemp.push(item.nhietdo);
 	})
-	if(dataLight.length > 12 || dataTime.length > 12){
-		dataLight.splice(6, 38);
-		dataTime.splice(6, 38);
+	if(dataLight.length > 10 || dataTime.length > 10 || dataTemp.length > 10){
+		dataLight.splice(6, 40);
+		dataTime.splice(6, 40);
+		dataTemp.splice(6 ,40);
 	}
 	let data1 = JSON.stringify(dataJson);
+	console.log(dataTemp)
 	fs.writeFileSync('dataCharts.json',data1);
 	return JSON.parse(fs.readFileSync('dataCharts.json','utf8'));
 }
--- public/xuly.charts.js
+++ public/xuly.charts.js
@@ -1,4 +1,4 @@
-const socket = io("http://nam2351998.herokuapp.com/");
+const socket = io("http://localhost:3484/");
 const ctx = document.getElementById('myChart').getContext('2d');
 $(document).ready(function(){
     socket.emit("getDataCharts");
@@ -7,27 +7,40 @@ $(document).ready(function(){
             type: 'line',
             data: {
                 labels: data[1].dataTime,
-                datasets: [{
-                    label: 'Nhiệt độ',
-                    data: data[0].dataLight,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+                datasets: [
+                    {
+                        label: 'lux',
+                        data: data[0].dataLight,
+                        backgroundColor: [
+                            'rgba(255,0,0,0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(47, 101, 63, 1)'
+                        ],
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Nhiệt độ',
+                        data: data[2].dataTemp,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }
+            ]
             },
             options: {
                 scales: {
