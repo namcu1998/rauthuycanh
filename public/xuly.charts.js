3d78605d147ebf5bb4022e5a9de5e72b3bdabd65
Namcu1998 <namcu1998@gmail.com>
2020-06-14 22:35:03 +0700

    make it better

--- public/xuly.charts.js
+++ public/xuly.charts.js
@@ -1,89 +1,101 @@
 const socket = io("http://nam2351998.herokuapp.com/");
 const ctx = document.getElementById('myChart').getContext('2d');
-function addData(chart, label, data, data1, data2) {
+const ctx1 = document.getElementById('myChart1').getContext('2d');
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+function addData(chart, chart1, chart2, label, data, data1, data2) {
     chart.data.labels.push(label);
+    chart1.data.labels.push(label);
+    chart2.data.labels.push(label);
     chart.data.datasets[0].data.push(data);
-    chart.data.datasets[1].data.push(data1);
-    chart.data.datasets[2].data.push(data2);
+    chart1.data.datasets[0].data.push(data1);
+    chart2.data.datasets[0].data.push(data2);
     chart.update();
+    chart1.update();
+    chart2.update();
 }
-function removeData(chart) {
+function removeData(chart, chart1, chart2) {
     chart.data.labels.shift();
+    chart1.data.labels.shift();
+    chart2.data.labels.shift();
     chart.data.datasets[0].data.shift();
-    chart.data.datasets[1].data.shift();
-    chart.data.datasets[2].data.shift();
+    chart1.data.datasets[0].data.shift();
+    chart2.data.datasets[0].data.shift();
     chart.update();
+    chart1.update();
+    chart2.update();
 }
 $(document).ready(function(){
     socket.emit("getDataCharts");
     socket.on("onCharts",function(data){
         data.map((item) => {
-            addData(myChart, item.dataTime, item.dataLight, item.dataTemp, item.dataHumi);
+            addData(myChart, myChart1,myChart2, item.dataTime, item.dataLight, item.dataTemp, item.dataHumi);
         })
     });
     socket.on("hmm",function(data){
-        addData(myChart, data[0].thoigian, data[0].light, data[0].nhietdo, data[0].doam);
-        removeData(myChart);
+        addData(myChart, myChart1, myChart2, data[0].thoigian, data[0].light, data[0].nhietdo, data[0].doam);
+        removeData(myChart, myChart1, myChart2);
     });
 }); //document
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
         labels: [],
         datasets: [
             {
                 label: 'light',
                 data: [],
-                backgroundColor: 'rgba(196, 245, 0, 1)',
+                backgroundColor: 'rgba(196, 245, 0, 0.8)',
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(141, 78, 78, 1)',
-                    'rgba(105, 58, 58, 1)',
-                    'rgba(71, 26, 26, 1)',
-                    'rgba(26, 38, 71, 1)',
+                    'rgba(196, 245, 0, 2)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)',
+                    // 'rgba(141, 78, 78, 1)',
+                    // 'rgba(105, 58, 58, 1)',
+                    // 'rgba(71, 26, 26, 1)',
+                    // 'rgba(26, 38, 71, 1)',
                 ],
                 borderWidth: 1
-            },
+            }
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                        return value + "LUX";
+                    }
+                }
+            }]
+        }
+    }
+});
+var myChart1 = new Chart(ctx1, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [
             {
-                label: 'Nhiệt độ',
+                label: 'nhiệt độ',
                 data: [],
-                backgroundColor: 'rgba(245, 0, 0, 1)',
+                backgroundColor: 'rgba(255,0,0, 0.5)',
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(141, 78, 78, 1)',
-                    'rgba(105, 58, 58, 1)',
-                    'rgba(71, 26, 26, 1)',
-                    'rgba(26, 38, 71, 1)',
+                    'rgba(255,0,0, 2)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)',
+                    // 'rgba(141, 78, 78, 1)',
+                    // 'rgba(105, 58, 58, 1)',
+                    // 'rgba(71, 26, 26, 1)',
+                    // 'rgba(26, 38, 71, 1)',
                 ],
-                borderWidth: 1
-            },
-            {
-                label: 'độ ẩm',
-                data: [],
-                backgroundColor: 'rgba(139, 97, 255, 1)',
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(141, 78, 78, 1)',
-                    'rgba(105, 58, 58, 1)',
-                    'rgba(71, 26, 26, 1)',
-                    'rgba(26, 38, 71, 1)',
-                ],
-                borderWidth: 1
+                borderWidth: 3
             }
     ]
     },
@@ -91,14 +103,50 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    fontSize: 20
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                        return value + "°C";
+                    }
                 }
-            }],
-            xAxes: [{
+            }]
+        }
+    }
+});
+var myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [
+            {
+                label: 'độ ẩm',
+                data: [],
+                backgroundColor: 'rgba(139, 97, 255, 0.8)',
+                borderColor: [
+                    'rgba(139, 97, 255, 2)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)',
+                    // 'rgba(141, 78, 78, 1)',
+                    // 'rgba(105, 58, 58, 1)',
+                    // 'rgba(71, 26, 26, 1)',
+                    // 'rgba(26, 38, 71, 1)',
+                ],
+                borderWidth: 3
+            }
+    ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
                 ticks: {
-                    fontSize: 12,
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                        return value + "%";
+                    }
                 }
-            }],
+            }]
         }
     }
 });
