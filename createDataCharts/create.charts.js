9971b410b71feacf7a7628495bae489a1a749901
Namcu1998 <namcu1998@gmail.com>
2020-06-15 07:36:53 +0700

    make it better

--- createDataCharts/create.charts.js
+++ createDataCharts/create.charts.js
@@ -3,18 +3,20 @@ module.exports =  function() {
 	var dataJson = [];
 	let data1 = JSON.parse(fs.readFileSync('data.json','utf8'));
 	data1.map(function(item){
+		let x = item.thoigian.split(" ");
+		let y = x[2].split(":");
 		let data = {
 			dataLight: item.light,
-			dataTime: item.thoigian,
+			dataTime: y[2],
 			dataTemp: item.nhietdo,
 			dataHumi: item.doam,
 		}
 		dataJson.push(data);
 	})
-	if(dataJson.length > 9 ){
-		dataJson.splice(9, 41);
+	if(dataJson.length > 50 ){
+		dataJson.splice(50, 1);
 	}
-	let data2 = JSON.stringify(dataJson);
+	let data2 = JSON.stringify(dataJson.reverse());
 	fs.writeFileSync('dataCharts.json',data2);
 	return JSON.parse(fs.readFileSync('dataCharts.json','utf8'));
 }
