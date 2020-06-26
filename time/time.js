b78a472c509c27c2a5e43c5c7c83339e9cfc3806
Namcu1998 <namcu1998@gmail.com>
2020-06-26 12:14:50 +0700

    make it better

--- time/time.js
+++ time/time.js
@@ -34,8 +34,43 @@ function timeDay(){
     return array;
   }
 
+  function getTime(){
+    var ngay
+    var array = [];
+    var day = moment().tz("Asia/Ho_Chi_Minh").format('dddd');
+    switch(day){
+      case 'Monday':
+        ngay = 'thứ hai'
+        break;
+      case 'Tuesday':
+        ngay = 'thứ ba'
+        break;
+      case 'Wednesday':
+        ngay = 'thứ tư'
+        break;
+      case 'Thursday':
+        ngay = 'thứ năm'
+        break;
+      case 'Friday':
+        ngay = 'thứ sáu'
+        break;
+      case 'Saturday':
+        ngay = 'thứ bảy'
+        break;
+      case 'Sunday':
+        ngay = 'chủ nhật'
+        break;
+    }
+    array[0] = ngay;
+    var time = moment().tz("Asia/Ho_Chi_Minh").format("HH:mm:ss");
+    array[1] = time.split(":");
+    var date = moment().tz("Asia/Ho_Chi_Minh").format('DD-MM-YYYY');
+    array[2] = date.split("-");
+    return ngay + date + time;
+  }
 function time(){
   return moment().tz("Asia/Ho_Chi_Minh").format("HH:mm");
 }
 module.exports.time = time;
 module.exports.timeDay = timeDay;
+module.exports.getTime = getTime;
