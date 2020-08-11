4e92cdd94e2b477f6be0d5823cc0f421cc297fa2
Namcu1998 <namcu1998@gmail.com>
2020-08-11 13:41:11 +0700

    firt

--- public/js/xuly.js
+++ public/js/xuly.js
@@ -51,9 +51,9 @@ $(document).ready(function(){
 		$("#setTemp")[0].value = data[1].setTemp;
 		$("#timeStop")[0].value = data[1].speakerTimeStop;
 		$("#timeStart")[0].value = data[1].speakerTimeStart;
-		xulyData("speaker", data[2].speaker);
-		xulyData("fanHumi", data[2].fanHumi);
-		xulyData("fanTemp", data[2].fanTemp);
+		// xulyData("speaker", data[2].speaker);
+		// xulyData("fanHumi", data[2].fanHumi);
+		// xulyData("fanTemp", data[2].fanTemp);
 		data[1].speakerDay.map((item) => {
 			let input = document.querySelectorAll("#auto input");
 			for(let x = 0 ; x < input.length ; x++){
