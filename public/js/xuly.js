167adbf6d2dc0d63315436e4411ceb5fd6c08e9c
Namcu1998 <namcu1998@gmail.com>
2020-08-11 13:24:15 +0700

    add button fan

--- public/js/xuly.js
+++ public/js/xuly.js
@@ -1,7 +1,7 @@
 const socket = io("https://bonghoaxinh.herokuapp.com/nam2351998"); 
 let test = 0;
 function xulyden(item1, item2){
-	if(item1 == 1){
+	if(item1 == 1){hdjfd
 		item2.bootstrapToggle('on');
 	}
 	else item2.bootstrapToggle('off');
@@ -51,6 +51,9 @@ $(document).ready(function(){
 		$("#setTemp")[0].value = data[1].setTemp;
 		$("#timeStop")[0].value = data[1].speakerTimeStop;
 		$("#timeStart")[0].value = data[1].speakerTimeStart;
+		xulyData("speaker", data[2].speaker);
+		xulyData("fanHumi", data[2].fanHumi);
+		xulyData("fanTemp", data[2].fanTemp);
 		data[1].speakerDay.map((item) => {
 			let input = document.querySelectorAll("#auto input");
 			for(let x = 0 ; x < input.length ; x++){
