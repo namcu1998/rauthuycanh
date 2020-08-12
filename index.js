bf94ff32379c2ab7aa065c388f6a2c202b9cd24f
Namcu1998 <namcu1998@gmail.com>
2020-08-12 09:44:49 +0700

    fix

--- index.js
+++ index.js
@@ -117,18 +117,12 @@ nsp.on('connection', function(socket){
 				console.log("bat humi")
 			}
 			if(data.humi > ma.getAuto().setHumi[0] && (data.fan) != 1){
+				if(ma.getAll()[2].fan == 1)
 				ma.fan(1);
 				ma.fanHumi(0);
 				nsp.emit("LED", ma.getAll()[2]);
 				webapp.emit("onMa1", ma.getAll()[2]);
 				console.log("bat fan")
-			}
-			if(data.humi > ma.getAuto().setHumi[1] && data.humi < ma.getAuto().setHumi[0] && ((data.fanHumi) != 0 || (data.fan) != 0)){
-				ma.fanHumi(0);
-				ma.fan(0);
-				nsp.emit("LED", ma.getAll()[2]);
-				webapp.emit("onMa1", ma.getAll()[2]);
-				console.log("all off")
 			}
 			if(data.temp > ma.getAuto().setTemp[0] && data.fan != 1){
 				ma.fan(1);
@@ -142,9 +136,10 @@ nsp.on('connection', function(socket){
 				nsp.emit("LED", ma.getAll()[2]);
 				webapp.emit("onMa1", ma.getAll()[2]);
 			}
-			if(data.temp > ma.getAuto().setTemp[1] && data.temp < ma.getAuto().setTemp[0] && (data.fanTemp != 0 || data.fan != 0)){
+			if(((data.temp > ma.getAuto().setTemp[1] && data.temp < ma.getAuto().setTemp[0]) || (data.humi > ma.getAuto().setHumi[1] && data.humi < ma.getAuto().setHumi[0])) && ((data.fanHumi) != 0 || data.fanTemp != 0 || data.fan != 0)){
 				ma.fanTemp(0);
 				ma.fan(0);
+				ma.fanHumi(0);
 				nsp.emit("LED", ma.getAll()[2]);
 				webapp.emit("onMa1", ma.getAll()[2]);
 			}
