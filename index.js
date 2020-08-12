ae4fc8f5d71912df8897ddb31ab940e590bdf51f
Namcu1998 <namcu1998@gmail.com>
2020-08-12 09:32:26 +0700

    fix

--- index.js
+++ index.js
@@ -130,19 +130,19 @@ nsp.on('connection', function(socket){
 				webapp.emit("onMa1", ma.getAll()[2]);
 				console.log("all off")
 			}
-			if(data.temp > ma.getAuto().setTempMax && data.fan != 1){
+			if(data.temp > ma.getAuto().setTemp[0] && data.fan != 1){
 				ma.fan(1);
 				ma.fanTemp(0);
 				nsp.emit("LED", ma.getAll()[2]);
 				webapp.emit("onMa1", ma.getAll()[2]);
 			}
-			if(data.temp < ma.getAuto().setTempMin && data.fanTemp != 1){
+			if(data.temp < ma.getAuto().setTemp[1] && data.fanTemp != 1){
 				ma.fanTemp(1);
 				ma.fan(0);
 				nsp.emit("LED", ma.getAll()[2]);
 				webapp.emit("onMa1", ma.getAll()[2]);
 			}
-			if(data.temp > ma.getAuto().setTempMin && data.temp < ma.getAuto().setTempMax && (data.fanTemp != 0 || data.fan != 0)){
+			if(data.temp > ma.getAuto().setTemp[1] && data.temp < ma.getAuto().setTemp[0] && (data.fanTemp != 0 || data.fan != 0)){
 				ma.fanTemp(0);
 				ma.fan(0);
 				nsp.emit("LED", ma.getAll()[2]);
