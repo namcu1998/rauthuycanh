7306e04c0884531f710c2beea2f7d0a7945de37d
Namcu1998 <namcu1998@gmail.com>
2020-08-16 10:41:13 +0700

    edit hitory

--- read.database/write.database.js
+++ read.database/write.database.js
@@ -1,20 +1,22 @@
 const fs  = require('fs')
 let newId;
-function Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, speaker, fanHumi, fanTemp){
+function Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, speaker, fanHumi, fanTemp, fan){
   this.nhietdo = nhietdo;
   this.doam = doam;
   this.light = light;
   this.thoigian = thing + " " + day + "/" + mouth + "/" + year + " " + hour + ":" + minute + ":" + second;
   this.fanHumi = fanHumi;
   this.speaker = speaker;
+  this.fanTemp = fanTemp;
+  this.fan = fan;
 }
-function fileSave(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, speaker, fanHumi, fanTemp){
+function fileSave(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, speaker, fanHumi, fanTemp, fan){
   if(fs.readFileSync('./JSON/data.json','utf8')) {
     var data = JSON.parse(fs.readFileSync('./JSON/data.json','utf8'))
     if(data.length > 100){
       data.splice(100,1);
     }
-    data.unshift(new Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, speaker, fanHumi, fanTemp));
+    data.unshift(new Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, speaker, fanHumi, fanTemp, fan));
     var data1 = JSON.stringify(data);
     fs.writeFileSync('./JSON/data.json',data1);
   }
