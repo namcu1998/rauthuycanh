5c65bf6f3312f19e04cdbc966978ce7cd0271cd3
Namcu1998 <namcu1998@gmail.com>
2020-08-16 22:23:54 +0700

    sua auto

--- public/js/xuly.lichsu.js
+++ public/js/xuly.lichsu.js
@@ -1,8 +1,12 @@
 const socket = io("https://nhayen.herokuapp.com/nam2351998"); //https://bonghoaxinh.herokuapp.com/nam2351998
 const table = $("#lich");
+function convert(data){
+	if(data == 1) return "Bật";
+	else return "Tắt";
+}
 function xulyData(getid, array){
 	var html = array.map(function(x){
-		return '<tr>' + '<td>' + x.light + "lx" + '</td>' + '<td>' + x.nhietdo + '*C' + '</td>' +'<td>' + x.doam + '%' +  '</td>' + '<td>' + x.thoigian + '</td>' + '<td>' + x.fanHumi + '</td>' + '<td>' + x.speaker + '</td>' + '<td>' + x.fanTemp + '</td>' + '<td>' + x.fan + '</td>' + '</tr>';
+		return '<tr>' + '<td>' + x.light + "lx" + '</td>' + '<td>' + x.nhietdo + '*C' + '</td>' +'<td>' + x.doam + '%' +  '</td>' + '<td>' + x.thoigian + '</td>' + '<td>' + convert(x.fanHumi) + '</td>' + '<td>' + convert(x.speaker) + '</td>' + '<td>' + convert(x.fanTemp) + '</td>' + '<td>' + convert(x.fan) + '</td>' + '</tr>';
 	});
 	var htmljoin = html.join('');
 	$("#lich").html(htmljoin);
