8b99d96a04626c66a03003a56731675e7a360319
Namcu1998 <namcu1998@gmail.com>
2020-09-01 13:09:53 +0700

    ok

--- public/js/xuly.tracuu.js
+++ public/js/xuly.tracuu.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
-  var newArray = []
   var long = 0;
-  var Array = []
   function convert(data){
     if(data == 1) return "Bật";
     else return "Tắt";
   }
   document.getElementById("start").addEventListener('input',function(){
+    var newArray = []
     long = $("#start").val().split("-").reverse().join("-");
     $.getJSON("https://nhayen-4b731.firebaseio.com/data.json", function(result){
       $.each(result, function(i, field){
@@ -16,8 +15,9 @@ $(document).ready(function(){
         }
       });
       if(newArray.length == 0){
-        alert("khong co du lieu")
+        document.getElementById("thongbao").innerHTML = "Không tìm thấy kết quả"
       }
+      else document.getElementById("thongbao").innerHTML = "Đã tìm thấy " + newArray.length + " kết quả"
       var html = newArray.map(function(x){
         return '<tr>' + '<th>'+ x[2] + "lx" + '</th>' + '<th>'+ x[0] + '</th>' + '<th>'+ x[1] + '</th>' + '<th>'+ x[3] + '</th>' + '<th>'+ convert(x[4]) + '</th>' + '<th>'+ convert(x[5]) + '</th>' + '<th>'+ convert(x[6]) + '</th>' + '<th>'+ convert(x[7]) + '</th>' + '</tr>';
       });
