cd35a39b1684ec967758e4429ad1f6bef92324a6
Namcu1998 <namcu1998@gmail.com>
2020-09-02 19:29:38 +0700

    ok

--- database/firebase.js
+++ database/firebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 const fs  = require('fs');
-var data = []
+var data2 = []
 var serviceAccount = require("../serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -20,8 +20,21 @@ var data1 = db.ref("arrayData")
 data1.on('child_added', function(snapshot) {
   var message=snapshot.val();
   // console.log(message)
+  data2.push(message)
   var array1 = JSON.stringify(message);
   fs.writeFileSync('./JSON/mode.auto.json',array1);
+  if(data2.length > 1) {
+    data2 = [];
+    data1.remove()
+      .then(function() {  
+        let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
+        data1.push(data)
+        console.log("Remove succeeded.")
+      })
+      .catch(function(error) {
+        console.log("Remove failed: " + error.message)
+      });
+  }
 });
 module.exports.data = data
 module.exports.data1 = data1
