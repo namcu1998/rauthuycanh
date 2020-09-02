60e37f2949894fceb955440c17515866f2162f24
Namcu1998 <namcu1998@gmail.com>
2020-09-02 18:57:47 +0700

    ok

--- database/firebase.js
+++ database/firebase.js
@@ -1,4 +1,6 @@
 var admin = require("firebase-admin");
+const fs  = require('fs');
+var data = []
 var serviceAccount = require("../serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -6,7 +8,8 @@ admin.initializeApp({
 });
 var number = 0;
 var db = admin.database();
-var ref = db.ref("data");
+var data = db.ref("data");
+var data1 = db.ref("arrayData")
 // ref.remove()
 //   .then(function() {
 //    console.log("Remove succeeded.")
@@ -14,10 +17,11 @@ var ref = db.ref("data");
 //   .catch(function(error) {
 //    console.log("Remove failed: " + error.message)
 //  });
-// ref.on('child_added', function(snapshot) {
-//   var message=snapshot.val();
-//   number = message.id
-//   console.log(message)
-//   fileSave(message.temp,message.humi,message.time,message.id)
-// });
-module.exports = ref
+data1.on('child_added', function(snapshot) {
+  var message=snapshot.val();
+  // console.log(message)
+  var array1 = JSON.stringify(message);
+  fs.writeFileSync('./JSON/mode.auto.json',array1);
+});
+module.exports.data = data
+module.exports.data1 = data1
--- modeAndDataAuto/create.mode.js
+++ modeAndDataAuto/create.mode.js
@@ -1,45 +1,53 @@
 const fs  = require('fs')
+const db = require('../database/firebase')
 function saveMode(mode){
     let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
     data[0] = mode;
     var array1 = JSON.stringify(data);
     fs.writeFileSync('./JSON/mode.auto.json',array1);
+    db.data1.push(data)
 }
 function saveAuto(auto){
     let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
     data[1] = auto;
     var array1 = JSON.stringify(data);
     fs.writeFileSync('./JSON/mode.auto.json',array1);
+    db.data1.push(data)
 }
 function fanHumi(fanHumi){
     let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
     data[2].fanHumi = fanHumi;
     var array1 = JSON.stringify(data);
     fs.writeFileSync('./JSON/mode.auto.json',array1);
+    db.data1.push(data)
 }
 function speaker(speaker){
     let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
     data[2].speaker = speaker;
     var array1 = JSON.stringify(data);
     fs.writeFileSync('./JSON/mode.auto.json',array1);
+    db.data1.push(data)
 }
 function fanTemp(fanTemp){
     let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
     data[2].fanTemp = fanTemp;
     var array1 = JSON.stringify(data);
     fs.writeFileSync('./JSON/mode.auto.json',array1);
+    db.data1.push(data)
 }
 function fan(fan){
     let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
     data[2].fan = fan;
     var array1 = JSON.stringify(data);
     fs.writeFileSync('./JSON/mode.auto.json',array1);
+    db.data1.push(data)
 }
 function statusEsp(esp){
     let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
     data[3] = esp;
     var array1 = JSON.stringify(data);
     fs.writeFileSync('./JSON/mode.auto.json',array1);
+    db.data1.push(data)
 }
 function setUpload(upLoad){
     let data = JSON.parse(fs.readFileSync('./JSON/mode.auto.json','utf8'))
