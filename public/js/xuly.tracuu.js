d0a23e59468cc72c7c73d4c4d566e2ed4c47e4a5
Namcu1998 <namcu1998@gmail.com>
2020-09-09 12:01:20 +0700

    ok

--- public/js/xuly.tracuu.js
+++ public/js/xuly.tracuu.js
@@ -1,28 +1,68 @@
+var long = 0;
+var scope = 0;
+var newArray = [];
+var nam = [];
+function chunkArray(myArray, chunk_size){
+    var results = [];
+
+    while (myArray.length) {
+        results.push(myArray.splice(0, chunk_size));
+    }
+
+    return results;
+}
+function convert(data){
+	if(data == 1) return "Bật";
+	else return "Tắt";
+}
+function MM(item){
+	if(scope < 0){
+		scope = 0;
+	}
+	if(scope > item){
+		scope = item;
+	}
+}
 $(document).ready(function(){
-  var long = 0;
-  function convert(data){
-    if(data == 1) return "Bật";
-    else return "Tắt";
-  }
-  document.getElementById("start").addEventListener('input',function(){
-    var newArray = []
-    long = $("#start").val().split("-").reverse().join("-");
-    $.getJSON("https://nhayen-4b731.firebaseio.com/data.json", function(result){
-      $.each(result, function(i, field){
-        var newData = field[3].split(" ")
-        if(newData[2] == long){
-          newArray.push(field);
-        }
-      });
-      if(newArray.length == 0){
-        document.getElementById("thongbao").innerHTML = "Không tìm thấy kết quả"
-      }
-      else document.getElementById("thongbao").innerHTML = "Đã tìm thấy " + newArray.length + " kết quả"
-      var html = newArray.map(function(x){
-        return '<tr>' + '<th>'+ x[2] + "lx" + '</th>' + '<th>'+ x[0] + '</th>' + '<th>'+ x[1] + '</th>' + '<th>'+ x[3] + '</th>' + '<th>'+ convert(x[4]) + '</th>' + '<th>'+ convert(x[5]) + '</th>' + '<th>'+ convert(x[6]) + '</th>' + '<th>'+ convert(x[7]) + '</th>' + '</tr>';
-      });
-      var htmljoin = html.join('');
-      $("#lich").html(htmljoin);
-})
-  });
+	function page(item) {
+		newArray = [];
+		long = $("#start").val().split("-").reverse().join("-");
+		$.getJSON("https://nhayen-4b731.firebaseio.com/data.json", function(result){
+			$.each(result, function(i, field){
+			var newData = field[3].split(" ")
+			if(newData[2] == long){
+				newArray.push(field);
+			}
+			});
+			if(newArray.length == 0){
+				document.getElementById("thongbao").innerHTML = "không tìm thấy kết quả"
+			}
+			else {
+				document.getElementById("thongbao").innerHTML = "đã tìm thấy " + newArray.length + " kết quả"
+				nam = chunkArray(newArray, 1000)
+				var nam1 = nam[item];
+				var html = nam1.map(function(x){
+					return '<tr>' + '<th>'+ x[2] + "lx" + '</th>' + '<th>'+ x[0] + '</th>' + '<th>'+ x[1] + '</th>' + '<th>'+ x[3] + '</th>' + '<th>'+ convert(x[4]) + '</th>' + '<th>'+ convert(x[5]) + '</th>' + '<th>'+ convert(x[6]) + '</th>' + '<th>'+ convert(x[7]) + '</th>' + '</tr>';
+				});
+				var htmljoin = html.join('');
+				$("#lich").html(htmljoin);
+				console.log("true")
+			}
+		})
+	}
+	document.getElementById("butonNext").addEventListener('click',function(){
+		scope++;
+		MM(nam.length - 1);
+		page(scope);
+		window.scrollTo(0, 0);
+	})
+	document.getElementById("butonPrevious").addEventListener('click',function(){
+		scope--;
+		MM(nam.length);
+		page(scope);
+		window.scrollTo(0, 0);
+	})
+	document.getElementById("start").addEventListener('input',function(){
+		page(0);
+	});
 });
