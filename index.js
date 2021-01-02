c1f7d239067ffb8ca4e7fb185b61a822eaa6d5a2
Namcu1998 <namcu1998@gmail.com>
2021-01-02 20:33:18 +0700

    test

--- index.js
+++ index.js
@@ -195,14 +195,16 @@ function loopSync() {
       }
       //Kiểm tra esp kết nối lại
       if (
-        (getAll().statusDevice.Device.Device === array.espControll[0] ||
-          getAll().statusDevice.Device.Device1 === array.espControll[1] ||
-          getAll().statusDevice.Device.Device2 === array.espControll[2] ||
-          getAll().statusDevice.Device.Device3 === array.espControll[3] ||
-          getAll().statusDevice.Device.Device4 === array.espControll[4] ||
-          getAll().statusDevice.Device.Device5 === array.espControll[5]) &&
+        (getAll().statusDevice.Device.Device != array.espControll[0] ||
+          getAll().statusDevice.Device.Device1 != array.espControll[1] ||
+          getAll().statusDevice.Device.Device2 != array.espControll[2] ||
+          getAll().statusDevice.Device.Device3 != array.espControll[3] ||
+          getAll().statusDevice.Device.Device4 != array.espControll[4] ||
+          getAll().statusDevice.Device.Device5 != array.espControll[5]) &&
         array.espControll.length > 0
       ) {
+      }
+      else {
         espControll.emit("LED", getAll().statusDevice.Device);
         console.log("kiểm tra device");
       }
