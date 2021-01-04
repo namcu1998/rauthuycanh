df55a4e1c14403c30bbe1c0bf4c29cfae1f2a2d7
Namcu1998 <namcu1998@gmail.com>
2021-01-04 09:34:39 +0700

    make it better

--- read.database/write.database.js
+++ read.database/write.database.js
@@ -14,8 +14,7 @@ function Object(nhietdo, doam, light, second, minute, hour, thing, day, mouth, y
   this.device5 = device5;
 }
 function fileSave(nhietdo, doam, light, second, minute, hour, thing, day, mouth, year, device, device1, device2, device3, device4, device5){
-  let thoigian = thing + " " + day + "/" + mouth + "/" + year + " " + hour + ":" + minute + ":" + second;
-  dulieuDb.push({nhietdo, doam, light, thoigian, device, device1, device2, device3, device4, device5});
+  dulieuDb.push([[nhietdo, doam, light, thoigian, device, device1, device2, device3, device4, device5]]);
   if(fs.readFileSync('./JSON/data.json','utf8')) {
     var data = JSON.parse(fs.readFileSync('./JSON/data.json','utf8'))
     if(data.length > 100){
